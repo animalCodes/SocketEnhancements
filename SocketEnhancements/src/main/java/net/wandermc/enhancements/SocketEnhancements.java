8e2df9a056ef2244b7700187e0bd291b5224bd1c
animalCodes <84966313+animalCodes@users.noreply.github.com>
2024-01-12 19:13:50 +0000

    Initial commit
    Project layout, POMs and license.

---
+++ SocketEnhancements/src/main/java/net/wandermc/enhancements/SocketEnhancements.java
@@ -0,0 +1,25 @@
+/*
+ *    SocketEnhancements: A basic gear enhancement plugin for PaperMC servers.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package net.wandermc.enhancements;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SocketEnhancements extends JavaPlugin {
+    public void onEnable() {
+        this.getLogger().info("SocketEnhancements loaded!");
+    }
+}
